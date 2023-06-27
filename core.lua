275f1da6b46a2cfe2377a9134f892ca15603e309
Larsj02 <l.m.johnen@gmail.com>
2023-06-28 00:31:51 +0200

    Added LibDeflate & changed format

--- core.lua
+++ core.lua
@@ -5,13 +5,6 @@ local function printMessage(msg, ...)
     print(string.format("%s: " .. msg, addonPrefix, ...))
 end
 
---[[
-    local lastData = {}
-    local lastTitle = {}
-    local active = false
-    local startTime = 0
-]]
-
 local openRaidLib = LibStub:GetLibrary("LibOpenRaid-1.0")
 
 local function Nova_IterateGroup(reversed, forceParty)
@@ -30,6 +23,22 @@ local function Nova_IterateGroup(reversed, forceParty)
     end
 end
 
+local function throwEvent(subEvent, data, db)
+    WeakAuras.ScanEvents("NOVA_INTERNAL", subEvent, data, db.active)
+end
+
+local function checkProgress(db)
+    if db.dungeonCount == db.maxDungeons then
+        db.active = false
+        local time = GetTime() - db.startTime
+        local formattedTime = string.format("%d:%02d", time / 60, time % 60)
+        local formattedCut = BreakUpLargeNumbers(db.lastTitle.cut)
+        local formattedCutPerHour = BreakUpLargeNumbers(db.lastTitle.cut / (time / 60 / 60))
+        printMessage("the Boost took %s and you made %s Gold (%s Gold per Hour)", formattedTime, formattedCut, formattedCutPerHour)
+        throwEvent("RESET", nil, db)
+    end
+end
+
 local function OnEvent(self, event, ...)
     if event == "ADDON_LOADED" then
         if ... == addonName then
@@ -39,16 +48,21 @@ local function OnEvent(self, event, ...)
                 printMessage("Boost Still Active from Last Session use /nb last to show last data")
             end
         end
+    elseif event == "CHALLENGE_MODE_START" then
+        if self.db.dungeonCount == 0 then
+            self.db.startTime = GetTime()
+        end
+    elseif event == "CHALLENGE_MODE_COMPLETED" then
+        self.db.dungeonCount = self.db.dungeonCount + 1
+        checkProgress(self.db)
     end
 end
 
 local f = CreateFrame("Frame")
 f:RegisterEvent("ADDON_LOADED")
+f:RegisterEvent("CHALLENGE_MODE_START")
+f:RegisterEvent("CHALLENGE_MODE_COMPLETED")
 f:SetScript("OnEvent", OnEvent)
-
-local function throwEvent(subEvent, data)
-    WeakAuras.ScanEvents("NOVA_INTERNAL", subEvent, data, f.db.active)
-end
 
 function f.OnGearUpdate()
     local db = f.db
@@ -62,7 +76,7 @@ function f.OnGearUpdate()
             data[unit] = {ilvl, score}
         end
     end
-    throwEvent("PLAYERS", data)
+    throwEvent("PLAYERS", data, db)
     db.lastPlayers = data
 end
 
@@ -74,36 +88,30 @@ SLASH_NBUI2 = "/boost"
 SlashCmdList.NBUI = function(msg)
     local db = f.db
     if msg == "reset" then
-        throwEvent("RESET")
+        throwEvent("RESET", nil, db)
     elseif msg == "players" then
         printMessage("Requesting Player Data")
         openRaidLib.RequestAllData()
-        if db.lastPlayers then throwEvent("PLAYERS", db.lastPlayers) end
+        if db.lastPlayers then throwEvent("PLAYERS", db.lastPlayers, db) end
     elseif msg == "last" then
-        throwEvent("PLAYERS", db.lastPlayers)
-        throwEvent("BOOST", db.lastTitle)
+        throwEvent("PLAYERS", db.lastPlayers, db)
+        throwEvent("BOOST", db.lastTitle, db)
     elseif msg == "start" then
         db.active = true
         db.startTime = GetTime()
         printMessage("Boost started")
     elseif msg == "stop" then
-        db.active = false
-        local time = GetTime() - db.startTime
-        local formattedTime = string.format("%d:%02d", time / 60, time % 60)
-        local formattedCut = BreakUpLargeNumbers(db.lastTitle.cut)
-        local formattedCutPerHour = BreakUpLargeNumbers(db.lastTitle.cut / (time / 60 / 60))
-        printMessage("the Boost took %s and you made %s Gold (%s Gold per Hour)", formattedTime, formattedCut, formattedCutPerHour)
-        throwEvent("RESET")
+        checkProgress(db)
     elseif msg == "help" then
         printMessage("/nb and /boost can be used as Prefix")
         printMessage("/nb help to show this message")
         printMessage("/nb reset to reset data")
-        printMessage("/nb <title> <cut> to update title and cut")
         printMessage("/nb players to force update players")
         printMessage("/nb last to show last data")
         printMessage("/nb start | stop to start or stop the boost")
         printMessage("/nb calc <cut> to calculate cut (15%%)")
         printMessage("/nb check <ilvl> to check if you can trade")
+        printMessage("/nb <Key Message> to update the title and cut")
     elseif string.match(msg, "^check") then
         local minIlvl = tonumber(string.match(msg, "(%d+)$"))
         local data = {}
@@ -116,13 +124,15 @@ SlashCmdList.NBUI = function(msg)
                 }
             end
         end
-        throwEvent("CHECK", data)
+        throwEvent("CHECK", data, db)
         printMessage("Checking for non tradeable Slots (ilvl < %d)", minIlvl)
     elseif string.match(msg, "^calc") then
-        local cut = string.match(msg, "(%d+)$")
+        local cut = string.match(msg, "(%d+)$"):gsub("%p", ""):gsub("[kK]", "000")
         printMessage("Your cut would be %s Gold", BreakUpLargeNumbers(tonumber(cut) * 0.15))
     else
-        local title, cut = string.match(msg, "(.-)%s+(%d+)$")
+        local title = string.match(msg, ":keystone_nova:%s+(.+):[ha]")
+        local count = string.match(title, "(%d+)x")
+        local cut = string.match(msg, ":goldss:%s+(.+)"):gsub("%p", ""):gsub("[kK]", "000")
         if (not title) or (not cut) then
             printMessage("Invalid Syntax use /nb help for more information")
             return
@@ -130,10 +140,13 @@ SlashCmdList.NBUI = function(msg)
         cut = tonumber(cut) * 0.15
         local data = {
             title = title,
-            cut = cut
+            cut = cut,
+            count = count
         }
-        throwEvent("BOOST", data)
+        throwEvent("BOOST", data, db)
         db.lastTitle = data
+        db.dungeonCount = 0
+        db.maxDungeons = tonumber(count) or 1
         printMessage("Set Title to %s and Cut to %s Gold", title, BreakUpLargeNumbers(cut))
     end
 end
